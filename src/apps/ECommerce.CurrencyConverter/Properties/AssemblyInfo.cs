c1cc26fb5338fdd4c9c347ea8a09e150e161b094
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-05-14 12:18:12 +0300

    final touches for first release

--- src/apps/ECommerce.CurrencyConverter/Properties/AssemblyInfo.cs
+++ src/apps/ECommerce.CurrencyConverter/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ECommerce.CurrencyConverter")]
-[assembly: AssemblyDescription("ECommerce currency converter project")]
+[assembly: AssemblyDescription("ECommerce currency converter project based on TCMB XML daily currency service")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tugberk Ugurlu")]
 [assembly: AssemblyProduct("ECommerce.CurrencyConverter")]
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1")]
-[assembly: AssemblyFileVersion("0.0.1")]
+[assembly: AssemblyVersion("0.1.0")]
