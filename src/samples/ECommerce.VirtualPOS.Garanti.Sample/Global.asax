97828614937e836a66c2deebaa31c2ba59ceb95b
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-05-14 13:12:52 +0300

    initing the sample proj for ECommerce.VirtualPOS.Garanti

---
+++ src/samples/ECommerce.VirtualPOS.Garanti.Sample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ECommerce.VirtualPOS.Garanti.Sample.MvcApplication" Language="C#" %>
---
+++ src/samples/ECommerce.VirtualPOS.Garanti.Sample/Scripts/_references.js
@@ -0,0 +1,3 @@
+﻿/// <reference path="jquery-1.7.2.js" />
+/// <reference path="jquery.validate.js" />
+/// <reference path="jquery.validate.unobtrusive.js" />
