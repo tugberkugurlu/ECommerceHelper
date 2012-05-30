5bf6a7fc9dd37246dd4f0d669290a4e95fed0f2a
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-05-30 12:57:41 +0300

    added some static methods inside CardInfo clas such as IsCreditCardAMEX, IsCreditCardVISA

--- src/apps/ECommerceHelper.Common/CardInfo.cs
+++ src/apps/ECommerceHelper.Common/CardInfo.cs
@@ -9,6 +9,56 @@ namespace ECommerceHelper.Common {
 
     public class CardInfo {
 
+        private static readonly int[] amexCardPrefixes = new[] { 34, 37 };
+        private static readonly int[] visaCardPrefixes = new[] { 4 };
+        private static readonly int[] mastercardCardPrefixes = new[] { 51, 55 };
+        private static readonly int[] discoverCardPrefixes = new[] { 6011, 644, 65 };
+
+        private static List<Card> _cards = getCards();
+
+        /// <summary>
+        /// Determines if the specified credit card is a card issued by a Turkish bank.
+        /// </summary>
+        /// <param name="creditCardNumber"></param>
+        /// <returns></returns>
+        public static bool IsCreditCardTurkish(string creditCardNumber) {
+
+            var creditCardBINNo = extractBINCode(creditCardNumber);
+            return (_cards.Any(card => card.BINNo == creditCardBINNo)) ? true : false;
+        }
+
+        /// <summary>
+        /// Determines if the specified credit card is an American Express credit card
+        /// </summary>
+        /// <param name="creditCardNumber"></param>
+        /// <returns></returns>
+        public static bool IsCreditCardAMEX(string creditCardNumber) {
+
+            return isSpecificCreditCard(creditCardNumber, amexCardPrefixes);
+        }
+
+        /// <summary>
+        /// Determines if the specified credit card is an Visa credit card
+        /// </summary>
+        /// <param name="creditCardNumber"></param>
+        /// <returns></returns>
+        public static bool IsCreditCardVISA(string creditCardNumber) {
+
+            return isSpecificCreditCard(creditCardNumber, visaCardPrefixes);
+        }
+
+        /// <summary>
+        /// Determines if the specified credit card is an MasterCard credit card
+        /// </summary>
+        /// <param name="creditCardNumber"></param>
+        /// <returns></returns>
+        public static bool IsCreditCardMasterCard(string creditCardNumber) {
+
+            return isSpecificCreditCard(creditCardNumber, mastercardCardPrefixes);
+        }
+
+        //private helpers
+
         private static string getXMLContent() {
 
             using (Stream stream = typeof(CardInfo).Assembly.GetManifestResourceStream(string.Format("ECommerce.Common.Resources.{0}", "CardsInfo.xml")))
@@ -28,17 +78,27 @@ namespace ECommerceHelper.Common {
             return deseserializeCards();
         }
 
+        /// <summary>
+        /// The fist 6 digits are the Issuer Identification Number. It will identify the institution that issued the card.
+        /// </summary>
+        /// <param name="creditCardNumber"></param>
+        /// <returns></returns>
         private static string extractBINCode(string creditCardNumber) {
 
             return creditCardNumber.Substring(0, 6);
         }
 
-        public static bool IsCreditCardTurkish(string creditCardNumber) {
+        private static bool isSpecificCreditCard(string creditCardNumber, int[] prefixes) {
 
-            var cards = getCards();
             var creditCardBINNo = extractBINCode(creditCardNumber);
 
-            return (cards.Any(card => card.BINNo == creditCardBINNo)) ? true : false;
+            foreach (var prefix in prefixes) {
+                if (creditCardBINNo.StartsWith(prefix.ToString())) {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
